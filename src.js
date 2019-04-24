590fc4577fc3221ed399b821bbff5c1ad6864441
theterminallyillone <theterminallyillone@gmail.com>
2019-04-24 12:28:57 -0400

    Finished the design for pagination without overflow.

--- src.js
+++ src.js
@@ -18,11 +18,15 @@ function init() {
 	document.getElementById("date").innerHTML = dateString;
 	show(page);
 }
+function remove() {
+	document.getElementById("content").innerHTML = ""
+	document.getElementById("showremoved").style="";
+}
 function show(page) {
 	if (pages[page].title.length > 0) {
 		for (let i = 0; i < pages[page].title.length; i++)	{
 			if (pages[page].title[i] != undefined) {
-				let insert = "<div id='pg"+page.toString()+"c"+i.toString()+"' class='text'><h1>"+pages[page].title[i].replace("-", " ")+" ["+pages[page].extension[i]+"] "+pages[page].city[i]+"<div style='float:right'>"+pages[page].date[i]+"</div></h1><button id='pg"+page.toString()+"b"+i.toString()+"' onclick=\"document.getElementById('pg"+page.toString()+"b"+i.toString()+"').style='display:none';document.getElementById('pg"+page.toString()+"obj"+i.toString()+"').style='';document.getElementById('pg"+page.toString()+"tex"+i.toString()+"').style='';\">Display</button>";
+				let insert = "<div id='pg"+page.toString()+"c"+i.toString()+"' class='text'><h1>"+pages[page].title[i].replace(/-/g, " ")+" ["+pages[page].extension[i]+"] "+pages[page].city[i]+"<div style='float:right'>"+pages[page].date[i]+"</div></h1><button id='pg"+page.toString()+"b"+i.toString()+"' onclick=\"document.getElementById('pg"+page.toString()+"b"+i.toString()+"').style='display:none';document.getElementById('pg"+page.toString()+"obj"+i.toString()+"').style='';document.getElementById('pg"+page.toString()+"tex"+i.toString()+"').style='';\">Display</button>";
 				insert+="<div id='pg"+page.toString()+"tex"+i.toString()+"' style='display:none'><h2><pre>&#9"+pages[page].text[i]+"</pre><h2></div>";
 				if (pages[page].extension[i] == "png" || pages[page].extension[i] == "jpg" || pages[page].extension[i] == "jpeg" || pages[page].extension[i] == "gif") {
 					insert += "<img id='pg"+page.toString()+"obj"+i+"' style='display:none' src='"+URL+"assets/"+page.toString()+"/"+pages[page].title[i]+"_"+pages[page].date[i]+"."+pages[page].extension[i]+"'>";
@@ -32,6 +36,8 @@ function show(page) {
 					insert += "<audio controls style='display:none' id='pg"+page.toString()+"obj"+i.toString()+"'><source src='assets/"+page.toString()+"/"+pages[page].title[i]+"_"+pages[page].date[i]+"."+pages[page].extension[i]+"' type='audio/"+pages[page].extension[i]+"'></audio>";
 				} else if (pages[page].extension[i] == "mp4") {
 					insert += "<video controls style='display:none' id='pg"+page.toString()+"obj"+i.toString()+"'><source src='assets/"+page.toString()+"/"+pages[page].title[i]+"_"+pages[page].date[i]+"."+pages[page].extension[i]+"' type='video/"+pages[page].extension[i]+"'>/video>";
+				} else if (pages[page].extension[i] == "txt") {
+					insert += "<div id='pg"+page.toString()+"obj"+i.toString()+"'></div>";
 				}
 				insert += "</div>";
 				document.getElementById("content").insertAdjacentHTML("beforeend", insert);
@@ -41,6 +47,8 @@ function show(page) {
 			let insert = "<center class='text'><h1>No more content has been uploaded.</h1></center>";
 			document.getElementById("content").insertAdjacentHTML("beforeend", insert);
 			document.getElementById("showmore").style = "display:none";
+		} else {
+			document.getElementById("showmore").style="";
 		}
 	} else {
 		let insert = "<center class='text'><h1>No content has been uploaded.</h1></center>";
