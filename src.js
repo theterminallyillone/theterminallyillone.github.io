4e08e2ac50a87bceb137b22527a4860fc84cfc82
theterminallyillone <theterminallyillone@gmail.com>
2019-04-25 11:40:48 -0400

    Fixed yet another pagination issue.

--- src.js
+++ src.js
@@ -29,13 +29,13 @@ function show(page) {
 				let insert = "<div id='pg"+page.toString()+"c"+i.toString()+"' class='text'><h1>"+pages[page].title[i].replace(/-/g, " ")+" ["+pages[page].extension[i]+"] "+pages[page].city[i]+"<div style='float:right'>"+pages[page].date[i]+"</div></h1><button id='pg"+page.toString()+"b"+i.toString()+"' onclick=\"document.getElementById('pg"+page.toString()+"b"+i.toString()+"').style='display:none';document.getElementById('pg"+page.toString()+"obj"+i.toString()+"').style='';document.getElementById('pg"+page.toString()+"tex"+i.toString()+"').style='';\">Display</button>";
 				insert+="<div id='pg"+page.toString()+"tex"+i.toString()+"' style='display:none'><h2><pre>&#9"+pages[page].text[i]+"</pre><h2></div>";
 				if (pages[page].extension[i] == "png" || pages[page].extension[i] == "jpg" || pages[page].extension[i] == "jpeg" || pages[page].extension[i] == "gif") {
-					insert += "<img id='pg"+page.toString()+"obj"+i+"' style='display:none' src='"+URL+"assets/"+page.toString()+"/"+pages[page].title[i]+"_"+pages[page].date[i]+"."+pages[page].extension[i]+"'>";
+					insert += "<img id='pg"+page.toString()+"obj"+i+"' style='display:none' src='"+URL+"assets/"+pages[page].title[i]+"_"+pages[page].date[i]+"."+pages[page].extension[i]+"'>";
 				} else if (pages[page].extension[i] == "pdf") {
-					insert += "<object type='application/pdf' id='pg"+page.toString()+"obj"+i.toString()+"'style='display:none' data='assets/"+page.toString()+"/"+pages[page].title[i]+"_"+pages[page].date[i]+"."+pages[page].extension[i]+"'></object>";
-				} else if (pages[page].extension[i] == "mp3") {
-					insert += "<audio controls style='display:none' id='pg"+page.toString()+"obj"+i.toString()+"'><source src='assets/"+page.toString()+"/"+pages[page].title[i]+"_"+pages[page].date[i]+"."+pages[page].extension[i]+"' type='audio/"+pages[page].extension[i]+"'></audio>";
+					insert += "<object type='application/pdf' id='pg"+page.toString()+"obj"+i.toString()+"'style='display:none' data='assets/"+pages[page].title[i]+"_"+pages[page].date[i]+"."+pages[page].extension[i]+"'></object>";
+				} else if (pages[page].extension[i] == "mp3" || pages[page].extension[i] == "wav") {
+					insert += "<audio controls style='display:none' id='pg"+page.toString()+"obj"+i.toString()+"'><source src='assets/"+pages[page].title[i]+"_"+pages[page].date[i]+"."+pages[page].extension[i]+"' type='audio/"+pages[page].extension[i]+"'></audio>";
 				} else if (pages[page].extension[i] == "mp4") {
-					insert += "<video controls style='display:none' id='pg"+page.toString()+"obj"+i.toString()+"'><source src='assets/"+page.toString()+"/"+pages[page].title[i]+"_"+pages[page].date[i]+"."+pages[page].extension[i]+"' type='video/"+pages[page].extension[i]+"'>/video>";
+					insert += "<video controls style='display:none' id='pg"+page.toString()+"obj"+i.toString()+"'><source src='assets/"+pages[page].title[i]+"_"+pages[page].date[i]+"."+pages[page].extension[i]+"' type='video/"+pages[page].extension[i]+"'>/video>";
 				} else if (pages[page].extension[i] == "txt") {
 					insert += "<div id='pg"+page.toString()+"obj"+i.toString()+"'></div>";
 				}
