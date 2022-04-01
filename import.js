e3638768ab7a59b90fcc84d28e6e3d863317d897
theterminallyillone <theterminallyillone@gmail.com>
2022-04-01 07:34:02 -0400

    static offline serving is done

--- import.js
+++ import.js
@@ -128,30 +128,7 @@ function init() {
 									}
   							});
     					}
-    					});});
-							/*if (req.url === "/") {
-								fs.readFile("./index.html", "UTF-8", function(err, html) {
-									res.writeHead(200, {"Content-Type": "text/html"});
-									res.end(html);
-								});
-							} else {
-								var matched = "false";
-								for (var i = 0; i < contentExts.length; i++) {
-									if (req.url.match("\."+contentExts[i]+"	$")) {
-										matched = contentTypes[i];
-										console.log(matched);	
-									}
-								}
-								if (matched != "false") {
-										var fileStream = fs.createReadStream("./", "UTF-8");
-										res.writeHead(200, {"Content-Type": matched	});
-										fileStream.pipe(res);
-								} else {
-										res.writeHead(404, {"Content-Type": "text/html"});
-										res.end("No Page Found");
-								}
-							}
-						});*/
+    				});});
 						server.listen(PORT);
 						rl.resume();
 						rl.on('SIGINT', () => {
